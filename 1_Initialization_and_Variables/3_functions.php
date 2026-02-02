810eef5f2831daf80ad88329d3f6ad60b9609b0c
Iago Colombo <iagocbrandao@gmail.com>
2026-02-01 21:58:40 -0300

    New topics to Understand what i'm doing

---
+++ 1_Initialization_and_Variables/3_functions.php
@@ -0,0 +1,39 @@
+<?php
+
+// In this file, we gonna talk and see abou Functions,
+// The commum way to reuse some piece of code!
+
+// Here we gonna see how to declare:
+
+$name; // Variable in usage
+
+function hello($name) {
+    echo "Hello, " . $name . "!"; 
+}
+
+// Functions, are declared with function reserved word
+// And with this, we could let a name of Function and
+// Necessary Variables, like {$name}, check return:
+
+$name = "Iago";
+hello($name);
+echo "<br>";
+
+// -----------------------------------------------
+
+// We can make different things with functions, 
+// Like sum, validation etc. To make simple, let's
+// Make a sum Function:
+
+function add($n1,$n2) {
+    return $n1 + $n2;
+}
+
+// To get this information, we can attribuite on
+// A variable, to demonstrate to user:
+
+$result = add(16,26);
+var_dump($result);
+echo "<br>";
+
+// In future, we gonna see more about Aritmatic usage!
---
+++ 1_Initialization_and_Variables/README.md
@@ -0,0 +1,10 @@
+# 1 Topic - Initialization and Variables
+This topic, we gonna see initial things about programming in PHP
+
+-----------
+
+Talking more about it, we gonna see:
+1. Variables (Types and Extra: echo/var_dump)
+2. More About Arrays (Numeric X Associative and Extras)
+3. Functions (How declare and difference)
+4. Aritmatic Logic (Types and usage)
